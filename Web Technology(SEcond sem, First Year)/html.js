c6bde8d36e8f587445100732216e14ee90659307
WangWangAustin <92380477+Thomaz29@users.noreply.github.com>
2023-01-17 10:54:26 +0630

    Add files via upload

---
+++ Web Technology(SEcond sem, First Year)/html.js
@@ -0,0 +1,18 @@
+
+        function test()
+        {
+            document.getElementById("P1").style.color="red";
+            document .body .style.backgroundColor="pink";
+        }
+        function test1()
+        {
+            document .getElementById("P1").style.color="blue";
+           
+            alert("Hello")
+
+        }
+      function test2()
+        { 
+            document.getElementById("P2").style.color="white";
+            document.body .style.backgroundColor="green";
+        }
